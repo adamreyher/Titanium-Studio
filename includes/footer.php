5642896a170ecae8492e6e5c1fdab68669c8b442
Adam Reyher <adamr@phpbb.com>
2011-06-10 00:25:46 -0400

    w3schools lied to me! The rel should be license instead of copyright.

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 			<div id="footer">
 				<div id="copyright">
-					<img src="/images/icons/cc-16.png" title="Creative Commons BY 3.0" alt="CC BY 3.0" /> Titanium Studio • CC BY 3.0 • <a href="/license/" rel="copyright">Credits &amp; Source Code</a> | <a href="http://validator.w3.org/check?uri=referer">Valid HTML5</a>
+					<img src="/images/icons/cc-16.png" title="Creative Commons BY 3.0" alt="CC BY 3.0" /> Titanium Studio • CC BY 3.0 • <a href="/license/" rel="license">Credits &amp; Source Code</a> | <a href="http://validator.w3.org/check?uri=referer">Valid HTML5</a>
 				</div>
 				<div id="f-contact-info">
 					<a href="http://www.facebook.com/pages/Titanium-Studio/314090419751"><img src="/images/icons/facebook-16.png" alt="Add us on Facebook" title="Add us on Facebook" /></a>&nbsp; <a href="http://twitter.com/titaniumstudio"><img src="/images/icons/twitter-16.png" alt="Follow us on Twitter" title="Follow us on Twitter" /></a> • Ph +1 813 466 8480 •  <a href="mailto:info@titaniumstudio.com" class="nofade">info@titaniumstudio.com</a>
